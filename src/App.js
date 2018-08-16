705d20a54f832fba30d568d08558c412e8f44ae3
Priya Hiremath <priyahrish18@gmail.com>
2018-08-16 22:55:09 +1200

    Edited

--- src/App.js
+++ src/App.js
@@ -19,34 +19,49 @@ class App extends Component {
   </div>
   
 </div>
-<section className="section $section-padding is-medium">
+<div className="section $section-padding is-medium"></div>
 <div className="container">
-  <h1 className="title">Blog</h1>
-  <h2 className="subtitle">
-  I have been one of the lucky one who got the chance to go to ScaleConf which was held on 11th of August here in Wellington.
+  
+  
+  I consider myself one of the lucky one who got the chance to go to ScaleConf which was held on 7th of August here in Wellington.
 
-  As much as I was looking forward to attend my first ever tech conference, I was also looking forward to  hear from  some of the smartest people from around  the world. 
-  At last the day arrived, and i landed in Te Papa there were beautiful team of volunteers waiting to welcome you and register with some small goodies to give away.
-   Morning started with Introduction speech by lovely host Mr..””
-  He was the perfect host for the day with his excellent humour and knowledge,  day started with small introduction speech and we were sent to sit  in one of the halls in Te papa.
-  There was long list of speakers  line up, I was very curious to listen to 1) google guy
-  2)the girl from twitter
-  3. Man from AWs
-  4. Trade me guy
-  5. Blockchain person
-  All of the speakers were great in there own field.
-   Some of the topics were easy to understand and some them were totally new but felt like I have added lot of knowledge in to my Brain in the whole day.
-  My particular favourite was the speech from google guy.
+  As much as I was looking forward to attend my first ever tech conference, I was more interested in listening from some of the smartest people from around the world. 
+  The venue was our most beautiful and architurally strong building "Te Papa"  we were welcomed by some very enthusiatic volunteers at the entrance  and they handed over 
+  some goodies and registered us for the day.
+    Our host for the day had a preety good sense of humour and good knowledge about all the speakers.
+     I remember when he was giving some in case of emergency instructions, he said if Tusanmi then follow him to upstairs and get better views from there, and incase of earthquake sit tight for rock and roll.
+     as we are in the one of the safest and strongest building in the wellington" 
+     The day had good line up of speakers it started with scaling the clouds and finished with Data patterns. There were lot to take from every speaker,
+     seeing their passion and work made me wonder "Technology is very deep side of this new age and how we grow and fit in here will only be decided by our drive and passion"
+     Whole day I got to hear about so many new frameworks, systems and all sort of new technology cool words ;) 
+     Well I must admit though not everything stuck in my brain, in some of the speeches I was completly LOST, As my tech jouny hasnt been so deep yet,
+     for eg. Heptio Contour, world of ingress and service mesh, data Patterns etc..
+     after every speech my brain came up with the question, Like what is this?? and how it works?? where is technolgoy heading towards?
+     and Oh MY GOD how am I supoosed to know all this?? 
+     well.. well.. well.. Don't be afraid we are not in the race, with the time I am sure we all will know all these things, this just our 
+     beginning of the new age high tech world. We all should be excited.. about this journey as there is so much out there it's like a sea.
+     Our day was fully packed back to back speeches, in lunch time my favorite one's were: Blockchain by Mark pascall A I really wanted to know what the 
+     the whole big fuss Blockchain is all about. I was really looking forward to listening to Vinu from Twitter but with some visa issues she couldn't make it.
+     so that was bit a shame.
+     Also enjoyed the talk of Mr.Kumar while he talked about the avilability and downtime formula about the websites, and Million dollar webpage.
+     Also it was quite interesting to hear Mr. Daniel McClelland talkig about automation tests. I also got to talk to him in a person.
+      It was a great opprtunity for all of us there to network, meet these brillient speakers and know about thier journey.
+      During Lunch time we had this chance to meet and greet all these people and get to know them.
+      I must mention here, that big appluse to all the catering team, The food was absolutly fantastic. I loved all the salad options for lunch with Rice, curry and Lamb kebabs. Also Pastrami, melted cheese Croissant for afternoon tea,
+      And rest of the food with all the veg and vegan options.
+
+      After lunch there were some more interesting topics and speakers were lined up some of them were Seth vargo, who talked about DevOps vs SRE.
+      Seth came from google. Peter Sbarski who came from Australia he talked about Serverless in Practice. rest of the talks were also interesting, I will write down summary for these talk below,
+
+      It was all worth attending the scale Conf for beggineer like me it was a great platform to,to know what's going around the world, where the tech industry is heading towards, what you can learn and how you can make technolgy safer for people around the world and how you can help to do that.
+       the knowledge,technolgoy, Networking. there were some more questions took birth in my brain,
+      it's like seeing other side of the world of technolgoy.</div>
+    
+
   
-  Also it was bit of a shame  that  we had to miss twiiter girl ,she could not make it because of some visa issue. Wished only she could record her speech and send it through you tube or something.
-   On the day the food was delicious there were good mix of choices for veg. Vegan and others. 
-  My fav was Lunch menu with three different types of salads and  afternoon tea with Pastrami with melted cheese with croissant.
-  
-  I think knowledge I added was learning about AWS, Data storage, Jenkins, automation testing, traversey, Devops vs R, whats the big fuss about blockchain and what does even it means also Learned about
-    </h2>
     <div><h1 className="title  is-primary is-centered">Lessons Learned</h1></div>
-      </div>
-      </section>
+      
+      
       
       <div className="columns is-centered inline-block is-one-third is-mobile $column-gap is-multiline box is-small">
       
@@ -55,11 +70,13 @@ class App extends Component {
        
         <img className="image is-128x128" src="../images/1.jpg" alt=""/>
         Andrew Jones
+        <img classname="image is-128-128" src="../images/DataP-Jones.jpg" alt=""/>
         
         </div>
         <div className="column is-one-fifth $column-gap">
         <img className="image is-128x128" src="../images/2.jpg" alt="" />
         Ben Dechrai
+        <img className="image is-128x128" src="../images/JStokens-ben.JPG" alt="" />
         </div>
         <div className="column is-one-third $column-gap">
         <img className="image is-128x128" src="../images/3.jpg" alt=""/>
@@ -130,8 +147,11 @@ class App extends Component {
         Seth Vargo
         </div>
         </div>
-      
+        <div>
         </div>
+        </div>
+      
+        
       
       
               
